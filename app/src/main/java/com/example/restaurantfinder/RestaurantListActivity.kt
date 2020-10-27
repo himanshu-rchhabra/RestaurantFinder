2e5abfa592687a9b2576ad9deb05db9e0ad62d67
hichha <hichha@microsoft.com>
2020-10-28 00:15:35 +0530

    show single item

--- app/src/main/java/com/example/restaurantfinder/RestaurantListActivity.kt
+++ app/src/main/java/com/example/restaurantfinder/RestaurantListActivity.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.Observer
 import com.example.restaurantfinder.ui.RestaurantList.RestaurantListViewModel
 import com.example.restaurantfinder.ui.RestaurantList.RestaurantListViewState
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.restaurant_item_row.*
 
 class RestaurantListActivity : AppCompatActivity() {
     private lateinit var viewModelFactory: RestaurantListViewModel.Factory
@@ -35,15 +36,26 @@ class RestaurantListActivity : AppCompatActivity() {
     private fun renderLoadingState() {
         loading_text.visibility = View.VISIBLE
         error_loading.visibility = View.GONE
+        restaurant_single_item.visibility = View.GONE
     }
 
     private fun renderErrorState() {
         loading_text.visibility = View.GONE
         error_loading.visibility = View.VISIBLE
+        restaurant_single_item.visibility = View.GONE
     }
 
     private fun renderData(viewState: RestaurantListViewState) {
         loading_text.visibility = View.GONE
         error_loading.visibility = View.GONE
+        restaurant_single_item.visibility = View.VISIBLE
+
+        viewState.restaurants?.let {
+            if (it.isNotEmpty()) {
+                restaurant_name.text = it.first().name
+                restaurant_cuisine.text = it.first().cuisines
+            }
+        }
+
     }
 }
