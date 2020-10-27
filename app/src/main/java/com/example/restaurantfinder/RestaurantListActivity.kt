46fe43757b50d7ab2d9e60aa776a290866b635d3
hichha <hichha@microsoft.com>
2020-10-27 12:09:06 +0530

    some activity stuff

--- app/src/main/java/com/example/restaurantfinder/RestaurantListActivity.kt
+++ app/src/main/java/com/example/restaurantfinder/RestaurantListActivity.kt
@@ -2,8 +2,12 @@ package com.example.restaurantfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.example.restaurantfinder.ui.RestaurantList.RestaurantListViewModel
+import com.example.restaurantfinder.ui.RestaurantList.RestaurantListViewState
+import kotlinx.android.synthetic.main.activity_main.*
 
 class RestaurantListActivity : AppCompatActivity() {
 
@@ -14,5 +18,35 @@ class RestaurantListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModelFactory = Injection.provideRestaurantListViewModelFactory(this)
+
+        viewModel.getLiveData().observe(this, Observer {
+            renderState(it)
+        })
+        viewModel.loadData()
+    }
+
+    private fun renderState(viewState: RestaurantListViewState) {
+        when {
+            viewState.isLoading -> renderLoadingState()
+            viewState.error != null -> renderErrorState()
+            else -> renderData(viewState)
+        }
+    }
+
+    private fun renderLoadingState() {
+        loading_text.visibility = View.VISIBLE
+        error_loading.visibility = View.GONE
+    }
+
+    private fun renderErrorState() {
+        loading_text.visibility = View.GONE
+        error_loading.visibility = View.VISIBLE
+    }
+
+    private fun renderData(viewState: RestaurantListViewState) {
+        loading_text.visibility = View.GONE
+        error_loading.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/restaurantfinder/ui/RestaurantList/RestaurantListViewModel.kt
+++ app/src/main/java/com/example/restaurantfinder/ui/RestaurantList/RestaurantListViewModel.kt
@@ -1,15 +1,34 @@
 package com.example.restaurantfinder.ui.RestaurantList
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.restaurantfinder.api.ZomatoApi
 import com.example.restaurantfinder.persistence.RestaurantDao
+import io.reactivex.disposables.CompositeDisposable
 
 class RestaurantListViewModel(
     private val database: RestaurantDao,
     private val api: ZomatoApi
 ) : ViewModel() {
 
+    private val liveData = MutableLiveData(RestaurantListViewState())
+    fun getLiveData(): LiveData<RestaurantListViewState> = liveData
+
+    private val disposable = CompositeDisposable()
+
+    fun loadData() {
+        // Dont reload data, if it is loading, or already loaded without errors
+        liveData.value?.let {
+            if (it.isLoading
+                || (it.error == null && !it.restaurants.isNullOrEmpty())
+            ) {
+                return
+            }
+        }
+
+    }
 
     open class Factory(
         private val database: RestaurantDao,
@@ -21,5 +40,10 @@ class RestaurantListViewModel(
                 api = this.api
             ) as T
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
